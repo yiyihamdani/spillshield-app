e8fa7523984e2a563c9a4e12d8bfc5bc3c5386d2
Yiyi Muhidin SH <120668094+yiyihamdani@users.noreply.github.com>
2024-01-02 00:23:40 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import pickle
+import numpy as np
+import streamlit as st
+
+from until import classify
+
+#set title 
+st.title('Klasifikasi Tumpahan Minyak')
+
+#set header
+st.header('Unggah Gambar')
+
+#upload file
+file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
+
+#load classifer
+model - load_model('./model/model.h5')
+
+"load class names
+with open('./model/labels-ril.txt', 'r') as f:
+  class_names = [a[:-1].split('')[1] for a in f.readlines()]
+  f.close()
+
+#display image
+if file is not None:
+  image = Image.open(file).convert('RGB')
+  st.image(image, use_column_width-True)
+
+  #classify image
+  class_name, conf_score - classify(image, model, class_name)
+
+  #write classification
+  st.write("## {}".format(class_name))
+  st.write("### Prediksi: {}%". format(int(conf_score *1000) / 10))
